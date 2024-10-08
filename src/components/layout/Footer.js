fefad265d76945e56240677258dd441dbda24992
Brokendil <divyanshusoni651@gmail.com>
2024-10-09 00:38:05 +0530

    redux and firebase adding

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -153,23 +153,11 @@ const Footer = () => {
                 , All right reserved.
               </span>
             </div>
-            <div className="col-md-6 my-auto text-center text-md-end text-white">
-              {/*/*** This template is free as long as you keep the below author’s credit link/attribution link/backlink. *** /*/}
-              {/*/*** If you'd like to use the template without the below author’s credit link/attribution link/backlink, *** /*/}
-              {/*/*** you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". *** /*/}
-              Designed By{" "}
-              <a className="border-bottom" href="https://htmlcodex.com">
-                HTML Codex
-              </a>{" "}
-              Distributed By{" "}
-              <a className="border-bottom" href="https://themewagon.com">
-                ThemeWagon
-              </a>
-            </div>
+          
           </div>
         </div>
       </div>
-      {/* Copyright End */}
+      
       {/* Back to Top */}
       <a
         href="#"
