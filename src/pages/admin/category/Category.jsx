7cdaa5d0f171b482265e3485ceb674b7aa4532a3
divyanshu <divyanshusoni651@gmail.com>
2025-07-20 22:44:53 +0530

    new commit

--- src/pages/admin/category/Category.jsx
+++ src/pages/admin/category/Category.jsx
@@ -2,7 +2,6 @@
 import React, { useEffect } from 'react'
 import { useSelector,useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { getCategory } from '../../../redux/sagas/category.saga'
 import { GET_NEW_CATEGORY } from '../../../redux/constants/categories.constant'
 
 const Category = () => {
